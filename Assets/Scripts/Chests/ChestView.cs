0d0ef4da86f56fb226a7bcf6648aee51b8db34ab
i_rk_jain <svsravikiran.9@gmail.com>
2023-10-03 11:45:39 +0530

    Currency Service
    Coins and Gems are handled by CurrencyService class.

--- Assets/Scripts/Chests/ChestView.cs
+++ Assets/Scripts/Chests/ChestView.cs
@@ -3,4 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChestView : MonoBehaviour {
+
+    public void OnChestOpen() {
+        CurrencyService.Instance.AddCoins(100);
+    }
+
 }
---
+++ Assets/Scripts/Currency/CurrencyService.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CurrencyService : MonoSingleton<CurrencyService> {
+
+    private int coins = 0;
+    private int gems = 0;
+
+    public int Coins { get { return coins; } }
+    public int Gems { get { return gems; } }
+
+    [SerializeField] private TextMeshProUGUI coinsText;
+    [SerializeField] private TextMeshProUGUI gemsText;
+
+    public void AddGemsAndCoins(int g, int c) {
+        AddCoins(c);
+        AddGems(g);
+    }
+
+    public void AddCoins(int c) {
+        coins += c;
+        // change ui thing also
+        coinsText.text = coins.ToString();
+    }
+
+    public void AddGems(int g) {
+        gems += g;
+        // change ui thing also
+        gemsText.text = gems.ToString();
+    }
+
+    public void MinusCoins(int c) {
+        coins -= c;
+        // change ui thing also
+    }
+
+    public void MinusGems(int g) {
+        gems -= g;
+        // change ui thing also
+    }
+
+}
